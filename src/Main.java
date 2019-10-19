0ad27741810311fd7ea10a778713044bb990ec47
luisguillermo <guillermo.velasco98@gmail.com>
2019-10-20 00:10:09 +0200

    Exercise 2, first commit.

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,30 @@ public class Main {
     public static final int BLUE_BOARD = 1500000;
     static HashMap<Integer, Element> myBoard;
 
-    public static void settingTheBoard(int size) {
+    public static void settingTheBoard(int size, int bombs) {
         // Setting the board
         myBoard = new HashMap<>();
 
-        for(int i = 0; i < size; i++) {
-            myBoard.put(i, new Space());
+        //Changed to doubles in order to get a ratio
+        double sizeDouble = size;
+        double bombsDouble = bombs;
+        double ratio = sizeDouble/bombsDouble;
+
+        // Depending on the ratio bombs:spaces the grid will be filled with spaces or bombs respectively
+        if(ratio <= 0.5) {
+            for(int i = 0; i < size; i++) {
+                myBoard.put(i, new Space());
+            }
+            // Adds the mines
+            minningTheBoard(bombs);
+        } else {
+            for(int i = 0; i < size; i++) {
+                myBoard.put(i, new Mine());
+            }
+            // Adds the spaces
+            spacingTheBoard(size-bombs);
         }
+
     }
 
     public static void minningTheBoard(int numberMines) {
@@ -30,17 +47,27 @@ public class Main {
         }
     }
 
+    public static void spacingTheBoard(int numberSpaces) {
+        Random random = new Random();
+        while (numberSpaces > 0) {
+            Integer trial = new Integer(random.nextInt(myBoard.size()));
+
+            if (myBoard.get(trial) instanceof Mine) {
+                myBoard.put(trial, new Space());
+                numberSpaces--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         long startingTime = System.currentTimeMillis();
-        settingTheBoard(RED_BOARD);
-        minningTheBoard(1500000); //
+        settingTheBoard(RED_BOARD, 1500000);
         long elapsedTime = (System.currentTimeMillis() - startingTime);
         System.out.println("1,500,000 bombs planted in " + elapsedTime + " milliseconds");
 
         startingTime = System.currentTimeMillis();
-        settingTheBoard(BLUE_BOARD);
-        minningTheBoard(1499999); //
+        settingTheBoard(BLUE_BOARD, 1499999);
         elapsedTime = (System.currentTimeMillis() - startingTime);
         System.out.println("1,499,999 bombs planted in " + elapsedTime + " milliseconds");
     }
